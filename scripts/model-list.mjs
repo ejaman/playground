c5b8a907c12bd877de10a61bb7cdd408b0ea2b7f
ejaman <leegm17@naver.com>
2026-02-25 11:27:12 +0900

    [Chore] script 실행 명령어 변경

--- scripts/model-list.mjs
+++ scripts/model-list.mjs
@@ -1,6 +1,6 @@
 import "dotenv/config";
 
-async function listModels() {
+async function modelList() {
   const API_KEY = process.env.GEMINI_API_KEY;
   const url = `https://generativelanguage.googleapis.com/v1beta/models?key=${API_KEY}`;
 

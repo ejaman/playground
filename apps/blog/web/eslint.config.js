b42bd0d8d80cd79e491dc6e4bc1db6b30a5ffb28
ejaman <leegm17@naver.com>
2026-03-16 12:34:37 +0900

    [Fix] 빌드 타입 오류 수정

--- apps/blog/web/eslint.config.js
+++ apps/blog/web/eslint.config.js
@@ -1,20 +1,4 @@
 import { nextJsConfig } from "@repo/eslint-config/next-js";
-import tailwindV4 from "eslint-plugin-tailwind-v4";
 
 /** @type {import("eslint").Linter.Config[]} */
-export default [
-  {
-    ignores: [".contentlayer/**"],
-  },
-  ...nextJsConfig,
-  {
-    plugins: { "tailwind-v4": tailwindV4 },
-    rules: {
-      // Tailwind v4: @theme 기준으로 정의된 클래스만 허용, 오타/미정의 클래스 린트 시 에러
-      "tailwind-v4/no-undefined-classes": [
-        "error",
-        { cssFile: "src/app/styles/globals.css" },
-      ],
-    },
-  },
-];
+export default [...nextJsConfig];

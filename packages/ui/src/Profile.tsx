26265538d13fd67a795ab0e2da655a0cb3f6a3ca
ejaman <leegm17@naver.com>
2026-03-16 13:01:10 +0900

    [Chore] 불필요한 린트 방지 제거

--- packages/ui/src/Profile.tsx
+++ packages/ui/src/Profile.tsx
@@ -46,7 +46,6 @@ export function Profile({
     <div className={cn("rounded-2xl  bg-card/50 p-6", "flex gap-6", className)}>
       {/* 프로필 이미지 */}
       <div className="shrink-0">
-        {/* eslint-disable-next-line */}
         <img
           src={image}
           alt={`${name} 프로필 이미지`}
